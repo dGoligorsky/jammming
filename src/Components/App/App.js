3f33faf601f7d8be52e3c7126b6f11843bca480b
David Goligorsky <d.goligorsky@gmail.com>
2020-04-20 12:11:17 -0400

    up to section 2

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
-import SearchBar from "../SearchBar/SearchBar.js";
-import SearchResults from "../SearchResults/SearchResults.js";
+import SearchBar from "../SearchBar/SearchBar";
+import SearchResults from "../SearchResults/SearchResults";
 import Playlist from "../Playlist/Playlist";
 
 class App extends React.Component {
