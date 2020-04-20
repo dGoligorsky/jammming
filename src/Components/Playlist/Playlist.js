dc3e92efc11d31c09253e8a38d1c4e6fe0567cd2
David Goligorsky <d.goligorsky@gmail.com>
2020-04-20 11:45:17 -0400

    got the first few components set up

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -4,12 +4,11 @@ import "./Playlist.css";
 class Playlist extends React.Component {
   render() {
     return (
-        <div className="Playlist">
-        <input value="New Playlist"/>
-        <!-- Add a TrackList component -->
+      <div className="Playlist">
+        <input value="New Playlist" />
+        {/* <!-- Add a TrackList component --> */}
         <button className="Playlist-save">SAVE TO SPOTIFY</button>
       </div>
-      
     );
   }
 }
