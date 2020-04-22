373ac418d2d71a6a5a1c158c42e767501cca844b
David Goligorsky <d.goligorsky@gmail.com>
2020-04-22 12:08:12 -0400

    up to step 73. still have some issue with rendering updated states for adding/removing tracks. states do update, but don't render changes.

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -3,12 +3,32 @@ import "./Playlist.css";
 import TrackList from "../TrackList/TrackList";
 
 class Playlist extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleNameChange = this.handleNameChange.bind(this);
+  }
+
+  handleNameChange(event) {
+    this.props.onNameChange(event.target.value);
+  }
+
   render() {
     return (
       <div className="Playlist">
-        <input value={this.props.playlistName} />
-        <TrackList tracks={this.props.playlistTracks} />
-        <button className="Playlist-save">SAVE TO SPOTIFY</button>
+        <input
+          defaultValue={"New Playlist"}
+          // value={this.props.playlistName}
+          onChange={this.handleNameChange}
+        />
+        <TrackList
+          tracks={this.props.playlistTracks}
+          onRemove={this.props.onRemove}
+          isRemoval={true}
+        />
+        <button className="Playlist-save" onClick={this.props.onSave}>
+          SAVE TO SPOTIFY
+        </button>
       </div>
     );
   }
